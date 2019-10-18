570d5a21323c2f35244776180936035047d7278c
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-10-18 12:49:47 +0100

    little more optimization

--- names/names.py
+++ names/names.py
@@ -5,18 +5,16 @@ start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
-binary_names_1 = BinarySearchTree(names_1[0])
-for name in names_1:
-    if name is not names_1[0]:
-        binary_names_1.insert(name)
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-binary_names_2 = BinarySearchTree(names_2[0])
-for name in names_2:
-    if name is not names_2[0]:
-        binary_names_2.insert(name)
+line_count = 0
+for line in f:
+    if line_count == 0:
+        binary_names_2 = BinarySearchTree(line[:-1])
+    else:
+        binary_names_2.insert(line)
+    line_count += 1
 f.close()
 
 # duplicates = []
@@ -26,14 +24,7 @@ f.close()
 #             duplicates.append(name_1)
 # Runtime for this solution is O(n^2)
 
-duplicates = []
-
-def append_dupplicates(value):
-    if binary_names_2.contains(value):
-        duplicates.append(value)
-
-binary_names_1.for_each(append_dupplicates)
-
+duplicates = [name for name in names_1 if binary_names_2.contains(name + "\n")]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
